b74a5d6b74621d29212bf42bb84fe0d864741455
Frank Gregor <phranck@woodbytes.me>
2021-05-16 22:59:31 +0200

    totally refined RadioStation model by removing Realm support

--- Sources/Model/RadioStation.swift
+++ Sources/Model/RadioStation.swift
@@ -5,90 +5,43 @@
 //
 
 import Foundation
-//import RealmSwift
 
-public struct RadioStation: Codable, Equatable {
+public struct RadioStation: Identifiable {
+    
+    public var id: String
+    public var name: String
+    public var streamUrl: String
+    public var website: String?
+    public var icon: String?
+    public var countryCode: String
+    public var codec: String?
+    public var clickCount: Int
+    public var votes: Int
+    public var bitrate: Int
 
-    var stationUUID: String = ModelDefaults.EmptyString
-    var name: String        = ModelDefaults.EmptyString
-    var url: String         = ModelDefaults.EmptyString
-    var streamUrl: String   = ModelDefaults.EmptyString
-    var website: String     = ModelDefaults.EmptyString
-    var icon: String        = ModelDefaults.EmptyString
-    var countryCode: String = ModelDefaults.EmptyString
-    var codec: String       = ModelDefaults.EmptyString
-    var supportsHLS: Int    = 0
-    var clickCount: Int     = 0
-    var votes: Int          = 0
-    var bitrate: Int        = 0
+}
+
+extension RadioStation: Codable {
     
     private enum CodingKeys: String, CodingKey {
-        case stationUUID   = "stationuuid"
+        case id            = "stationuuid"
         case name          = "name"
         case streamUrl     = "url_resolved"
         case website       = "homepage"
         case icon          = "favicon"
         case countryCode   = "countrycode"
         case codec         = "codec"
-        case supportsHLS   = "hls"
         case clickCount    = "clickcount"
         case votes         = "votes"
         case bitrate       = "bitrate"
     }
+
+}
+
+extension RadioStation: Equatable {
     
     public static func == (lhs: RadioStation, rhs: RadioStation) -> Bool {
-        return lhs.stationUUID == rhs.stationUUID
+        return lhs.id == rhs.id
     }
 
 }
-//
-//public class RadioStation: Object, Codable {
-//    var stationUUID: String = ModelDefaults.EmptyString
-//    var name: String        = ModelDefaults.EmptyString
-//    var url: String         = ModelDefaults.EmptyString
-//    var streamUrl: String   = ModelDefaults.EmptyString
-//    var website: String     = ModelDefaults.EmptyString
-//    var icon: String        = ModelDefaults.EmptyString
-//    var countryCode: String = ModelDefaults.EmptyString
-//    var codec: String       = ModelDefaults.EmptyString
-//    var supportsHLS: Int    = 0
-//    var clickCount: Int     = 0
-//    var votes: Int          = 0
-//    var bitrate: Int        = 0
-//
-//    private enum CodingKeys: String, CodingKey {
-//        case stationUUID   = "stationuuid"
-//        case name          = "name"
-//        case streamUrl     = "url_resolved"
-//        case website       = "homepage"
-//        case icon          = "favicon"
-//        case countryCode   = "countrycode"
-//        case codec         = "codec"
-//        case supportsHLS   = "hls"
-//        case clickCount    = "clickcount"
-//        case votes         = "votes"
-//        case bitrate       = "bitrate"
-//    }
-//
-//    public override static func primaryKey() -> String? {
-//        return "stationUUID"
-//    }
-//
-//    convenience required public init(from decoder: Decoder) throws {
-//        self.init()
-//
-//        let values    = try decoder.container(keyedBy: CodingKeys.self)
-//
-//        stationUUID   = try values.decode(String.self, forKey: .stationUUID)
-//        name          = try values.decode(String.self, forKey: .name)
-//        streamUrl     = try values.decode(String.self, forKey: .streamUrl)
-//        website       = try values.decode(String.self, forKey: .website)
-//        icon          = try values.decode(String.self, forKey: .icon)
-//        countryCode   = try values.decode(String.self, forKey: .countryCode)
-//        codec         = try values.decode(String.self, forKey: .codec)
-//        supportsHLS   = try values.decode(Int.self, forKey: .supportsHLS)
-//        clickCount    = try values.decode(Int.self, forKey: .clickCount)
-//        votes         = try values.decode(Int.self, forKey: .votes)
-//        bitrate       = try values.decode(Int.self, forKey: .bitrate)
-//    }
-//}
